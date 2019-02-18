1b65b820a81d3cc12934eda542c557903ae5b8e1
sehyun hwang <sehyunny6@gmail.com>
2019-02-18 21:02:38 +0800

    18-02-2019

---
+++ 0. review/script.js
@@ -0,0 +1,9 @@
+var1=document.getElementById('wbtn');
+var1.addEventListener('click',function() {
+	document.getElementById('this_is_target_haha').className='white';
+})
+
+var2=document.getElementById('bbtn');
+var2.addEventListener('click',function(){
+	document.getElementById('this_is_target_haha').className='black';
+})
