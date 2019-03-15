1a8b57578c865648d93b286ca552acc0ee3351b9
sehyun hwang <sehyunny6@gmail.com>
2019-03-15 21:48:56 +0800

    15-03-2019

--- 8. final review/index.php
+++ 8. final review/index.php
@@ -1,6 +1,5 @@
 <?php
-
-	$config=array(
+$config=array(
 		"host" => "localhost",
 		"duser" => "root",
 		"dpw" => "1234",
@@ -12,20 +11,43 @@
 		return $conn;
 	}
 	$conn=db_init($config["host"],$config["duser"],$config["dpw"],$config["dname"]);
-	$result=mysqli_query($conn,'SELECT * FROM topic');
-
-
-	while($row=mysqli_fetch_assoc($result)){
-		echo '<a href="index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a><br/>';
-	}
-
-	$id=$_GET['id'];
-	$sql = "SELECT topic.id,topic.title,topic.description,user.name,topic.created  FROM topic LEFT JOIN user ON topic.author=user.id WHERE topic.id=".$id;
-	$result=mysqli_query($conn,$sql);
-	$row=mysqli_fetch_assoc($result);
-	echo htmlspecialchars($row['title']).'<br/>';
-	echo htmlspecialchars($row['description']).'<br/>';
-	echo $row['created'].'<br/>';
-	echo $row['name'].'<br/>';
-
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<header>
+		<h1>생활코딩 JavaScript</h1>
+	</header>
+	<nav>
+		<ol>
+			<?php
+
+			$result=mysqli_query($conn,'SELECT * FROM topic');
+
+
+			while($row=mysqli_fetch_assoc($result)){
+				echo '<li><a href="index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>';
+		}
+			?>
+		</ol>
+	</nav>
+	<article>
+		<?php
+			$id=mysqli_real_escape_string($conn,$_GET['id']);
+			$sql = "SELECT topic.id,topic.title,topic.description,user.name,topic.created  FROM topic LEFT JOIN user ON topic.author=user.id WHERE topic.id=".$id;
+			$result=mysqli_query($conn,$sql);
+			$row=mysqli_fetch_assoc($result);
+		?>
+	
+		<h2><?=htmlspecialchars($row['title'])?></h2>
+		<div><?=$row['created']?> | <?=htmlspecialchars($row['name'])?></div>
+		<div><p><?=htmlspecialchars($row['description'])?></p></div>
+	
+	</article>
+</body>
+</html>
+
